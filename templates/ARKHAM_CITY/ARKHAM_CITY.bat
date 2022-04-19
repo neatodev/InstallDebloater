8e9740a67a3146a779f408601e6454cc04035528
EV3RGR33N <68407948+EV3RGR33N@users.noreply.github.com>
2022-04-19 12:49:39 +0200

    Updated Arkham City template
    Added deletion of startup movies

--- templates/ARKHAM_CITY/ARKHAM_CITY.bat
+++ templates/ARKHAM_CITY/ARKHAM_CITY.bat
@@ -1,2 +1,2 @@
 :: Make sure to set your game path in ARKHAM_CITY.ini correctly, then run this batch file.
-.\InstallDebloater.exe ARKHAM_CITY.ini
+.\InstallDebloater.exe ARKHAM_CITY\ARKHAM_CITY.ini
