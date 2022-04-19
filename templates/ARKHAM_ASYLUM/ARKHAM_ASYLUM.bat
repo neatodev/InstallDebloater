8f9065dc809d00084c3da5bba38352242a5c8221
EV3RGR33N <68407948+EV3RGR33N@users.noreply.github.com>
2022-04-19 12:32:28 +0200

    Updated Arkham Asylum template
    Added deletion of startup movies

--- templates/ARKHAM_ASYLUM/ARKHAM_ASYLUM.bat
+++ templates/ARKHAM_ASYLUM/ARKHAM_ASYLUM.bat
@@ -1,2 +1,2 @@
 :: Make sure to set your game path in ARKHAM_ASYLUM.ini correctly, then run this batch file.
-.\InstallDebloater.exe ARKHAM_ASYLUM.ini
+.\InstallDebloater.exe ARKHAM_ASYLUM\ARKHAM_ASYLUM.ini
