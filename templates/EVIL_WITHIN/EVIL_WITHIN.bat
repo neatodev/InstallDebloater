72fc95504f726cee976e83fadcf53cede4bfded2
EV3RGR33N <68407948+EV3RGR33N@users.noreply.github.com>
2022-07-24 21:14:37 +0200

    Update EVIL_WITHIN.bat

--- templates/EVIL_WITHIN/EVIL_WITHIN.bat
+++ templates/EVIL_WITHIN/EVIL_WITHIN.bat
@@ -1,2 +1,2 @@
-:: Make sure to set your game path in ARKHAM_ORIGINS.ini correctly, then run this batch file.
+:: Make sure to set your game path in EVIL_WITHIN.ini correctly, then run this batch file.
 .\InstallDebloater.exe EVIL_WITHIN\EVIL_WITHIN.ini
